c11c40168bfda0767c586548f8fc37a75881ea6d
Seb Green <sebastian_green@live.com>
2021-08-09 18:29:38 +0100

    backend tests

---
+++ server/__test__/integration.test.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = require('../Router');
+const cors = require('cors');
+const supertest = require('supertest');
+const db = require('../models/index');
+const Book = require('../models/Schema');
+
+
+describe('integration tests', () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(router);
+
+  const request = supertest(app);
+
+  // connect to db before...
+  beforeAll(async () => {
+    // connect to postgresql db / sequelize
+    const config = {host: 'localhost', dialect: 'postgres', logging: false};
+    const sequelize = new Sequelize('legacyread', 'postgres', '12345', config);
+  })
+
+  // delete objects (Books, etc.) to ensure 'clean-slate' each test run
+  afterAll(async () => {
+    // delete Book logic
+    await db.Book.destroy({where: {}, truncate: true });
+  })
+
+
+   describe('GET /', () => {
+   it('should get 200', (done) => {
+     request(app).get('/').expect(200, done);
+   });
+ });
+
+
+  it('should save a book to the database', async (done) => {
+    const title = "This is a test";
+    // title: req.body.volumeInfo.title,
+    const res = await request.postBook('/books')
+    // .expect(volumeInfo)
+//    .send({title})
+//   })
+
+  const book = db.Book.findOne({ where: { title = "This is a test"} });
+  expect(db.Book.title).toBe(title);
+  done();
+
+
+})
--- server/controllers/postBook.js
+++ server/controllers/postBook.js
@@ -1,6 +1,8 @@
 const db = require('../models/index');
 
 async function postBook(req, res) {
+  console.log(req.body);
+  
   try {
     const book = await db.Book.create({
       title: req.body.volumeInfo.title,
@@ -10,7 +12,8 @@ async function postBook(req, res) {
       image: req.body.volumeInfo.imageLinks.thumbnail,
       author: req.body.volumeInfo.author
     });
-
+    console.log(book);
+    
     res.status(201);
     res.send(book);
   } catch (err) {
